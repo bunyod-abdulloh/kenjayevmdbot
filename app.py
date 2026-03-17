49cdd0747cf3e9156e7bf02593431916b8ee3b9d
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2026-03-17 16:47:03 +0500

    Commited

--- app.py
+++ app.py
@@ -1,16 +1,49 @@
 from aiogram import executor
 
-from loader import dp, db
+from data.config import WEBHOOK_PATH, WEBAPP_HOST, WEBAPP_PORT, WEBHOOK_HOST
+from loader import dp, db, bot
 import middlewares, filters, handlers
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
 
 
+
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
+
 async def on_startup(dispatcher):
-    await set_default_commands(dispatcher)
-    await on_startup_notify(dispatcher)
+    try:
+        await set_default_commands(dispatcher)
+
+    except Exception as e:
+        pass
+
+    try:
+        await on_startup_notify(dispatcher)
+
+    except Exception as e:
+        pass
+
+    try:
+        await bot.set_webhook(WEBHOOK_URL)
+    except Exception as e:
+        pass
+
+
+async def on_shutdown(dispatcher):
+    # webhookni o‘chirib qo‘yamiz
+    await bot.delete_webhook()
 
 allowed_updates = ["message", "callback_query", "chat_join_request"]
 
-if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=on_startup, skip_updates=True, allowed_updates=allowed_updates)
+if __name__ == "__main__":
+    executor.start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        allowed_updates=allowed_updates,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
--- keyboards/default/user.py
+++
@@ -1,4 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup
-
-user_main_dkb = ReplyKeyboardMarkup(resize_keyboard=True)
-user_main_dkb.row("📱 Admin bilan aloqa")
