0051055337cc86e18226635ab83c7a8f3b5a7d09
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2026-03-17 21:38:47 +0500

    Commited

--- loader.py
+++ loader.py
@@ -3,12 +3,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from redis import Redis
 
 from data import config
-from data.config import REDIS_HOST, REDIS_PORT, REDIS_DB, REDIS_PASSWORD
-from utils.db_api.admins_db import AdminsDB
-from utils.db_api.create_tables import Database
-from utils.db_api.groups_db import GroupsDB
-from utils.db_api.referrals_db import ReferralsDB
-from utils.db_api.users_db import UsersDB
+from data.config import REDIS_HOST, REDIS_PORT, REDIS_DB
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
@@ -20,8 +15,3 @@ redis_client = Redis(
     decode_responses=True,  # string qaytarsin
 )
 dp = Dispatcher(bot, storage=storage)
-db = Database()
-udb = UsersDB(db)
-refdb = ReferralsDB(db)
-admdb = AdminsDB(db)
-grpdb = GroupsDB(db)
